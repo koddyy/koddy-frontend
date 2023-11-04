82939952a25f8cd0d36847b5e80ba9f4ae57c19c
juhee <77ooee@gmail.com>
2023-11-04 20:46:01 +0900

    style: global font 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import "@/styles/globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <link
+        rel="stylesheet"
+        as="style"
+        crossOrigin=""
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.css"
+      />
       <body>{children}</body>
     </html>
   );
