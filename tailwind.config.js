9b4b521ad868c8ac6f1bbbf08b243341eddbfede
kwonjuhee <62097867+kwonjuhee@users.noreply.github.com>
2023-11-04 23:47:20 +0900

    feat: theme 적용 및 Button 컴포넌트 제작 (#3)
    * style: theme colors 추가
    
    * style: global font 적용
    
    * feat: twMerge + clsx 유틸 함수 작성
    
    * feat: Button 컴포넌트 제작

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: "#0EBE5F",
+      secondary: "#F8BD20",
+      gray: {
+        100: "#F3F3F0",
+        200: "#E3E3E0",
+        300: "#CACAC9",
+        400: "#878787",
+        500: "#4B4B4B",
+        600: "#212121",
+      },
+    },
   },
   plugins: [],
 };
