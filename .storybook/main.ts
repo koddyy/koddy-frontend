1415374434257fcdcda04e7f2f60636f04135c5a
kwonjuhee <62097867+kwonjuhee@users.noreply.github.com>
2023-11-04 19:18:58 +0900

    chore: 프로젝트 초기 세팅 (#1)
    * chore: add eslint, prettier configuration
    
    * chore: add testing tools
    
    * chore: add storybook
    
    * chore: add simple-import-sort
    
    * chore: add svgr
    
    * chore: add tailwindcss
    
    * chore: add clsx, tailwind-merge
    
    * chore: add storybook path alias config
    
    * chore: 불필요한 파일 정리

---
+++ .storybook/main.ts
@@ -0,0 +1,46 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import * as path from "path";
+import type { RuleSetRule } from "webpack";
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+      nextConfigPath: path.resolve(__dirname, "../next.config.js"),
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, ".."),
+      };
+    }
+
+    if (!config.module || !config.module.rules) return config;
+
+    const imageRule = (config.module.rules as RuleSetRule[]).find(
+      (rule) => rule.test instanceof RegExp && rule.test.test(".svg")
+    );
+
+    if (imageRule) imageRule.exclude = /.svg$/;
+
+    config.module.rules.push({
+      test: /.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+export default config;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // https://react-svgr.com/docs/next/#usage
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
 
-module.exports = nextConfig
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
