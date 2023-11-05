91bbae065ff5c5a657781acf71dd7045db928c53
juhee <77ooee@gmail.com>
2023-11-05 19:57:34 +0900

    feat: Checkbox Group usecase 추가

--- components/Checkbox/Checkbox.stories.tsx
+++ components/Checkbox/Checkbox.stories.tsx
@@ -17,3 +17,32 @@ export const Default: Story = {
     children: <span>✔️</span>,
   },
 };
+
+const CheckboxGroup = () => {
+  const [isChecked, setIsChecked] = useState<Array<boolean>>(new Array(7).fill(false));
+
+  return (
+    <div className="flex gap-2">
+      {["월", "화", "수", "목", "금", "토", "일"].map((value, i) => (
+        <Checkbox
+          className="px-[0.8125rem] py-[0.6875rem]"
+          key={i}
+          checked={isChecked[i]}
+          onChangeChecked={() => {
+            setIsChecked((prev) => {
+              const cloned = [...prev];
+              cloned[i] = !cloned[i];
+              return cloned;
+            });
+          }}
+        >
+          {value}
+        </Checkbox>
+      ))}
+    </div>
+  );
+};
+
+export const Group: StoryObj<typeof CheckboxGroup> = {
+  render: () => <CheckboxGroup />,
+};
