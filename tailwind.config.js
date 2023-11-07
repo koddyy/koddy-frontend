7db8039526d8431741837e0f133a091941efaccf
juhee <77ooee@gmail.com>
2023-11-07 19:00:02 +0900

    style: z-index, color theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        lowest: -1,
+        overlay: 1000,
+        highest: 9999,
+      },
+    },
     colors: {
       primary: "#0EBE5F",
       secondary: "#F8BD20",
+      error: "#EC5757",
+      white: "#FFFFFF",
+      black: "#000000",
       gray: {
         100: "#F3F3F0",
         200: "#E3E3E0",
