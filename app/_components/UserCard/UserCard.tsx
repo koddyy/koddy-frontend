02937dd945b0c830a1c696165039d5a5fdff8ccd
juhee <77ooee@gmail.com>
2023-11-09 02:01:37 +0900

    feat: UserCard vertical 버전 추가

--- app/_components/UserCard/UserCard.tsx
+++ app/_components/UserCard/UserCard.tsx
@@ -1,6 +1,9 @@
 import { Tag } from "@/components/Tag";
 
+type CardType = "horizontal" | "vertical";
+
 interface UserCardProps {
+  cardType?: CardType;
   imageUrl?: string;
   name: string;
   description: string;
@@ -9,11 +12,16 @@ interface UserCardProps {
 }
 
 export const UserCard = ({
+  cardType = "horizontal",
   imageUrl = "/images/mock_profile.png", // TODO: default image 교체
-  name,
-  description,
-  languages,
+  ...props
 }: UserCardProps) => {
+  if (cardType === "horizontal") return <HorizontalUserCard imageUrl={imageUrl} {...props} />;
+
+  return <VerticalUserCard imageUrl={imageUrl} {...props} />;
+};
+
+const HorizontalUserCard = ({ imageUrl, name, description, languages }: UserCardProps) => {
   return (
     <div className="flex justify-between gap-5 rounded-xl bg-gray-100 p-3">
       <div className="shrink-0 rounded-lg">
@@ -21,15 +29,43 @@ export const UserCard = ({
       </div>
       <div className="grow">
         <div className="mb-2 mt-1 border-b border-b-gray-200">
-          <span className="body-1-bold">{name}</span>
-          <p className="text-gray-600">{description}</p>
+          <span className="body-1-bold mb-[0.13rem]">{name}</span>
+          <p className="label-bold text-gray-600">{description}</p>
         </div>
         <div className="flex flex-wrap justify-end gap-2">
-          {languages.map((language) => (
-            <Tag key={language}>{language}</Tag>
-          ))}
+          {languages?.map((language) => <Tag key={language}>{language}</Tag>)}
         </div>
       </div>
     </div>
+  );
+};
+
+const VerticalUserCard = ({
+  imageUrl,
+  name,
+  description,
+  nationality,
+  languages,
+}: UserCardProps) => {
+  return (
+    <>
+      <div className="">
+        <img className="h-60 w-full object-cover" src={imageUrl} />
+      </div>
+      <div className="px-5 py-3">
+        <div className="mb-2">
+          <span className="headline-2">{name}</span>
+          <p className="body-3-bold">{description}</p>
+        </div>
+        <div className="flex justify-between">
+          <Tag variant="primary-dark">{nationality}</Tag>
+          <div className="flex flex-wrap justify-end gap-2">
+            {languages.map((language) => (
+              <Tag key={language}>{language}</Tag>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
