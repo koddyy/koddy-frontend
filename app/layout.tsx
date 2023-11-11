3fea42be4c722173a610ec7f7cdab063cc778e88
juhee <77ooee@gmail.com>
2023-11-11 21:51:47 +0900

    style: min-width 지정

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.css"
         />
       </head>
-      <body className="mx-auto max-w-screen-sm">
+      <body className="mx-auto min-w-[375px] max-w-screen-sm">
         <QueryClientProvider>{children}</QueryClientProvider>
       </body>
     </html>
