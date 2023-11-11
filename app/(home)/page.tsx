bfa2cfbeb5462f48a1869490d82375cb9f018d72
juhee <77ooee@gmail.com>
2023-11-11 21:56:41 +0900

    fix: flex-gap 적용을 위해 div 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -26,13 +26,13 @@ const Home = () => {
 const MentorHome = () => {
   return (
     <div className="flex flex-col gap-4 px-5 pb-[5.75rem] pt-[0.87rem]">
-      <>
+      <div>
         <div className="subheading-bold mb-3">예약을 신청한 멘티가 있어요</div>
         <Link href={`/coffeechat/${123456}`}>
           <UserCard {...MOCK_MENTEE} />
         </Link>
-      </>
-      <>
+      </div>
+      <div>
         <div className="subheading-bold mb-3">멘티 둘러보기</div>
         <div className="flex flex-col gap-[0.81rem]">
           {new Array(10).fill(0).map((_, i) => (
@@ -41,22 +41,22 @@ const MentorHome = () => {
             </Link>
           ))}
         </div>
-      </>
+      </div>
     </div>
   );
 };
 
 const MenteeHome = () => {
   return (
-    <div className="flex flex-col px-5 pb-[5.75rem] pt-[0.87rem]">
-      <>
+    <div className="flex flex-col gap-4 px-5 pb-[5.75rem] pt-[0.87rem]">
+      <div>
         <div className="subheading-bold mb-3">제안 온 커피챗</div>
         <Link href={`/coffeechat/${78910}`}>
           <UserCard {...MOCK_MENTEE} imageUrl="/images/mock_profile2.png" />
         </Link>
+      </div>
+      <div>
         <div className="subheading-bold mb-3">멘토 둘러보기</div>
-      </>
-      <>
         <div className="flex flex-col gap-[0.81rem]">
           {new Array(10).fill(0).map((_, i) => (
             <Link key={i} href={`/profile/${i + 11}`}>
@@ -64,7 +64,7 @@ const MenteeHome = () => {
             </Link>
           ))}
         </div>
-      </>
+      </div>
     </div>
   );
 };
