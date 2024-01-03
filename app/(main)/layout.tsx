cdb8432cc85d5053dccbe196538f507ac95a79e8
kwonjuhee <77ooee@gmail.com>
2024-01-04 00:11:51 +0900

    refactor: 페이지 접근 제한 제거

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,22 +1,19 @@
 import { CSSProperties } from "react";
 import { BottomNavigation } from "@/app/components/BottomNavigation/BottomNavigation";
-import PrivateRoute from "@/app/components/PrivateRoute";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <PrivateRoute>
-      <div
-        className="pb-[calc(var(--bottom-navigation-height)+1.5rem)]"
-        style={
-          {
-            ["--bottom-navigation-height"]: "3.625rem",
-          } as CSSProperties
-        }
-      >
-        {children}
-        <BottomNavigation />
-      </div>
-    </PrivateRoute>
+    <div
+      className="pb-[calc(var(--bottom-navigation-height)+1.5rem)]"
+      style={
+        {
+          ["--bottom-navigation-height"]: "3.625rem",
+        } as CSSProperties
+      }
+    >
+      {children}
+      <BottomNavigation />
+    </div>
   );
 };
 
