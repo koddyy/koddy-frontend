679ff9e6c82ec58d3058fff0ba8c603c294ccb76
kwonjuhee <77ooee@gmail.com>
2024-01-05 03:07:10 +0900

    chore: proxy 설정 제거

--- next.config.js
+++ next.config.js
@@ -26,15 +26,6 @@ const nextConfig = {
 
     return config;
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
