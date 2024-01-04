a6514a9b48cf79811f2ab575fe8970dd99af4652
kwonjuhee <77ooee@gmail.com>
2024-01-04 17:28:45 +0900

    chore: proxy 설정

--- next.config.js
+++ next.config.js
@@ -26,6 +26,15 @@ const nextConfig = {
 
     return config;
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
