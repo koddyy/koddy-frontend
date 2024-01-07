c77e9c44ddccf9cf5bffceba25b7d001e764caa3
kwonjuhee <77ooee@gmail.com>
2024-01-08 00:48:20 +0900

    feat: 요청 헤더의 authorization 필드에 access token 값을 설정한다.

--- apis/axios.ts
+++ apis/axios.ts
@@ -1,6 +1,17 @@
 import axios from "axios";
+import { useAuthStore } from "@/stores/auth";
 
 export const apiInstance = axios.create({
   baseURL: process.env.NODE_ENV === "development" ? "/" : process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
+});
+
+apiInstance.interceptors.request.use((config) => {
+  const { accessToken } = useAuthStore.getState();
+
+  if (accessToken) {
+    config.headers.setAuthorization(accessToken);
+  }
+
+  return config;
 });
