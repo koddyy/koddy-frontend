11eaed8ec9308fe7e9d22c8c2499e8e93bb1d4e0
kwonjuhee <77ooee@gmail.com>
2024-01-10 23:48:06 +0900

    feat: 최근에 로그인한 provider를 가리키는 tooltip을 띄운다

--- components/LoginButton/LoginButton.tsx
+++ components/LoginButton/LoginButton.tsx
@@ -1,6 +1,8 @@
 import { ComponentPropsWithoutRef } from "react";
 import Google from "@/assets/google.svg";
 import Kakao from "@/assets/kakao.svg";
+import { Tooltip } from "@/components/Tooltip";
+import { useProviderStore } from "@/stores/provider";
 import { cn } from "@/utils/cn";
 
 interface LoginButtonProps extends ComponentPropsWithoutRef<"button"> {
@@ -8,21 +10,27 @@ interface LoginButtonProps extends ComponentPropsWithoutRef<"button"> {
 }
 
 export const LoginButton = ({ provider, ...props }: LoginButtonProps) => {
+  const { selectedProvider, loggedIn } = useProviderStore();
+
+  const isRecentLoggedIn = loggedIn && selectedProvider === provider;
+
   return (
-    <button
-      className={cn(
-        "body-1-bold flex w-full items-center justify-between rounded-xl px-[18px] py-[13px]",
-        provider === "kakao" && "bg-[#FEE500]",
-        provider === "google" && "outline outline-gray-400"
-      )}
-      {...props}
-    >
-      {provider === "kakao" && <Kakao />}
-      {provider === "google" && <Google />}
-      <span className="grow text-center">
-        {provider === "kakao" && "카카오로 시작"}
-        {provider === "google" && "구글로 시작"}
-      </span>
-    </button>
+    <Tooltip content="최근에 로그인했어요" open={isRecentLoggedIn}>
+      <button
+        className={cn(
+          "body-1-bold flex w-full items-center justify-between rounded-xl px-[18px] py-[13px]",
+          provider === "kakao" && "bg-[#FEE500]",
+          provider === "google" && "outline outline-gray-400"
+        )}
+        {...props}
+      >
+        {provider === "kakao" && <Kakao />}
+        {provider === "google" && <Google />}
+        <span className="grow text-center">
+          {provider === "kakao" && "카카오로 시작"}
+          {provider === "google" && "구글로 시작"}
+        </span>
+      </button>
+    </Tooltip>
   );
 };
