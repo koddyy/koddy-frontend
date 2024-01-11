53be66622d7710ef7f32f57f2a3db105badbdec1
kwonjuhee <77ooee@gmail.com>
2024-01-11 17:41:22 +0900

    refactor: Toggle 인터페이스 확장 및 disabled 스타일 추가

--- components/Toggle/Toggle.tsx
+++ components/Toggle/Toggle.tsx
@@ -1,5 +1,5 @@
 import { cva, VariantProps } from "cva";
-import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef, PropsWithChildren } from "react";
 import { cn } from "@/utils/cn";
 
 export const ToggleVariants = cva("border-primary", {
@@ -15,7 +15,9 @@ export const ToggleVariants = cva("border-primary", {
   },
 });
 
-export interface ToggleProps extends VariantProps<typeof ToggleVariants> {
+export interface ToggleProps
+  extends ComponentPropsWithoutRef<"button">,
+    VariantProps<typeof ToggleVariants> {
   className?: string;
   pressed?: boolean;
   onChangePressed?: () => void;
@@ -27,16 +29,18 @@ export const Toggle = ({
   pressed,
   onChangePressed,
   children,
+  ...props
 }: PropsWithChildren<ToggleProps>) => {
   return (
     <button
       type="button"
       className={cn(
-        "body-2 rounded-[10px] border border-gray-300 px-[13px] py-[11px]",
+        "body-2 rounded-[10px] border border-gray-300 px-[13px] py-[11px] disabled:cursor-not-allowed disabled:text-gray-400",
         pressed && ToggleVariants({ variant }),
         className
       )}
       onClick={onChangePressed}
+      {...props}
     >
       {children}
     </button>
