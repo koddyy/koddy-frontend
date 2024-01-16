7f13ac5324c2a5d82672b8cfc680bb0545afabfa
kwonjuhee <77ooee@gmail.com>
2024-01-16 16:31:05 +0900

    refactor: deprecated 타입, 컴포넌트 제거

--- app/signup/types/menteeForm.ts
+++ app/signup/types/menteeForm.ts
@@ -1,22 +1,6 @@
-import * as mentorForm from "@/app/signup/types/mentorForm";
 import { Mentee } from "@/types/mentee";
 
-/**
- * =========================
- * @TODO remove
- * =========================
- */
-
-type FirstStepForm = mentorForm.FirstStepForm;
-
-type SecondStepForm = Omit<mentorForm.SecondStepForm, "grade">;
-
-type ThirdStepForm = Omit<mentorForm.ThirdStepForm, "zoomLink">;
-
-/**
- *  =========================
- */
-
-type SignupForm = Pick<Mentee, "interestSchool" | "interestMajor" | "nationality" | "languages">;
-
-export type { FirstStepForm, SecondStepForm, SignupForm, ThirdStepForm };
+export type SignupForm = Pick<
+  Mentee,
+  "interestSchool" | "interestMajor" | "nationality" | "languages"
+>;
