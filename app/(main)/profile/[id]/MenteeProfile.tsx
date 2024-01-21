062a436f88c84a32667cbb7e828f8f13eb238161
kwonjuhee <77ooee@gmail.com>
2024-01-21 15:28:02 +0900

    refactor: 멘토, 멘티 프로필 컴포넌트 분리
    - 유저 프로필 조회 api가 분리된다면 경로로 구분하기

---
+++ app/(main)/profile/[id]/MenteeProfile.tsx
@@ -0,0 +1,65 @@
+import { Button } from "primereact/button";
+import { useGetUserById } from "@/apis/user/hooks/useGetUserById";
+import { GoToLoginBottomSheet } from "@/app/components/GoToLoginBottomSheet";
+import { LinkButton } from "@/components/Button";
+import { useAuth } from "@/hooks/useAuth";
+import { PendingBottomSheet } from "../../coffeechat/components/PendingBottomSheet";
+import { ResultBottomSheet } from "../../coffeechat/components/ResultBottomSheet/ResultBottomSheet";
+import { UserCard } from "../../components/UserCard";
+import { useRequestCoffeeChat } from "../hooks/useRequestCoffeeChat";
+
+interface MenteeProfileProps {
+  menteeId: number;
+}
+
+export const MenteeProfile = ({ menteeId }: MenteeProfileProps) => {
+  const {
+    isPending,
+    isRequested,
+    openPendingBottomSheet,
+    closePendingBottomSheet,
+    requestCoffeeChat,
+  } = useRequestCoffeeChat();
+  const { data: user, isLoading } = useGetUserById(menteeId);
+  const { isAuthenticated, me } = useAuth();
+
+  if (isLoading) return null;
+
+  if (!user) return <div>존재하지 않는 멘티예요</div>;
+
+  return (
+    <>
+      <UserCard cardType="vertical" {...user} />
+      <div className="px-5 py-3">
+        <div className="body-3-bold mb-[0.38rem]">자기소개</div>
+        <p className="body-1 rounded-[0.625rem] border border-gray-300 px-[1.125rem] py-[0.6875rem]">
+          {user.introduction || "자기소개가 없습니다."}
+        </p>
+      </div>
+      <div className="fixed bottom-[var(--bottom-navigation-height)] left-1/2 z-overlay w-full max-w-screen-sm -translate-x-1/2 border-t border-t-gray-200 bg-white px-5 py-[0.69rem]">
+        <Button onClick={openPendingBottomSheet}>커피챗 제안하기</Button>
+      </div>
+      {isPending &&
+        (isAuthenticated ? (
+          <PendingBottomSheet
+            resultType="positive"
+            description={[`${user.name}님에게`, "커피챗을 제안하시겠습니까?"]}
+            onClickNo={closePendingBottomSheet}
+            onClickYes={() =>
+              requestCoffeeChat({ mentor: String(me!.id), mentee: String(menteeId) })
+            }
+          />
+        ) : (
+          <GoToLoginBottomSheet onClose={closePendingBottomSheet} />
+        ))}
+
+      {isRequested && (
+        <ResultBottomSheet
+          resultType="positive"
+          description={[`${user.name}님에게`, "커피챗을 제안하였습니다."]}
+          confirmButton={<LinkButton href="/">홈으로 돌아가기</LinkButton>}
+        />
+      )}
+    </>
+  );
+};
--- app/(main)/profile/[id]/page.tsx
+++ app/(main)/profile/[id]/page.tsx
@@ -2,15 +2,9 @@
 
 import { useRouter } from "next/navigation";
 import { useGetUserById } from "@/apis/user/hooks/useGetUserById";
-import { PendingBottomSheet } from "@/app/(main)/coffeechat/components/PendingBottomSheet";
-import { ResultBottomSheet } from "@/app/(main)/coffeechat/components/ResultBottomSheet/ResultBottomSheet";
-import { UserCard } from "@/app/(main)/components/UserCard";
-import { useRequestCoffeeChat } from "@/app/(main)/profile/hooks/useRequestCoffeeChat";
-import { GoToLoginBottomSheet } from "@/app/components/GoToLoginBottomSheet";
 import { NavigationBar } from "@/app/components/NavigationBar";
-import { Button, LinkButton } from "@/components/Button";
-import { useAuth } from "@/hooks/useAuth";
-import { useToggle } from "@/hooks/useToggle";
+import { MenteeProfile } from "./MenteeProfile";
+import { MentorProfile } from "./MentorProfile";
 
 const Page = ({ params }: { params: { id: string } }) => {
   const userId = Number(params.id);
@@ -28,100 +22,9 @@ const Page = ({ params }: { params: { id: string } }) => {
         onClickGoback={() => router.back()}
         backButtonColor="white"
       />
-      {user.role === "mentor" && <MentorProfile userId={userId} />}
-      {user.role === "mentee" && <MenteeProfile userId={userId} />}
+      {user.role === "mentor" && <MentorProfile mentorId={userId} />}
+      {user.role === "mentee" && <MenteeProfile menteeId={userId} />}
     </>
   );
 };
-
-interface ProfileProps {
-  userId: number;
-}
-
-const MenteeProfile = ({ userId }: ProfileProps) => {
-  const {
-    isPending,
-    isRequested,
-    openPendingBottomSheet,
-    closePendingBottomSheet,
-    requestCoffeeChat,
-  } = useRequestCoffeeChat();
-  const { data: user, isLoading } = useGetUserById(userId);
-  const { isAuthenticated, me } = useAuth();
-
-  if (isLoading) return null;
-
-  if (!user) return <div>존재하지 않는 멘티예요</div>;
-
-  return (
-    <>
-      <UserCard cardType="vertical" {...user} />
-      <div className="px-5 py-3">
-        <div className="body-3-bold mb-[0.38rem]">자기소개</div>
-        <p className="body-1 rounded-[0.625rem] border border-gray-300 px-[1.125rem] py-[0.6875rem]">
-          {user.introduction || "자기소개가 없습니다."}
-        </p>
-      </div>
-      <div className="fixed bottom-[var(--bottom-navigation-height)] left-1/2 z-overlay w-full max-w-screen-sm -translate-x-1/2 border-t border-t-gray-200 bg-white px-5 py-[0.69rem]">
-        <Button onClick={openPendingBottomSheet}>커피챗 제안하기</Button>
-      </div>
-      {isPending &&
-        (isAuthenticated ? (
-          <PendingBottomSheet
-            resultType="positive"
-            description={[`${user.name}님에게`, "커피챗을 제안하시겠습니까?"]}
-            onClickNo={closePendingBottomSheet}
-            onClickYes={() => requestCoffeeChat({ mentor: String(me!.id), mentee: String(userId) })}
-          />
-        ) : (
-          <GoToLoginBottomSheet onClose={closePendingBottomSheet} />
-        ))}
-
-      {isRequested && (
-        <ResultBottomSheet
-          resultType="positive"
-          description={[`${user.name}님에게`, "커피챗을 제안하였습니다."]}
-          confirmButton={<LinkButton href="/">홈으로 돌아가기</LinkButton>}
-        />
-      )}
-    </>
-  );
-};
-
-const MentorProfile = ({ userId }: ProfileProps) => {
-  const { data: user, isLoading } = useGetUserById(userId);
-  const { isAuthenticated } = useAuth();
-  const [isGoToLoginBottomSheet, toggleGoToLoginBottomSheet] = useToggle();
-
-  if (isLoading) return null;
-
-  if (!user) return <div>존재하지 않는 멘토예요</div>;
-
-  return (
-    <>
-      <UserCard cardType="vertical" {...user} />
-      <div className="px-5 py-[20px]">
-        <div className="body-3-bold mb-[0.38rem]">자기소개</div>
-        <p className="body-1 rounded-[0.625rem] border border-gray-300 px-[1.125rem] py-[0.6875rem]">
-          {user.introduction || "자기소개가 없습니다."}
-        </p>
-      </div>
-      <div className="fixed bottom-[var(--bottom-navigation-height)] left-1/2 z-overlay flex w-full max-w-screen-sm -translate-x-1/2 border-t border-t-gray-200 bg-white px-5 py-[0.69rem]">
-        <LinkButton
-          href={`/schedule?id=${userId}`}
-          onClick={(e) => {
-            if (!isAuthenticated) {
-              e.preventDefault();
-              toggleGoToLoginBottomSheet();
-            }
-          }}
-        >
-          커피챗 신청하기
-        </LinkButton>
-      </div>
-      {isGoToLoginBottomSheet && <GoToLoginBottomSheet onClose={toggleGoToLoginBottomSheet} />}
-    </>
-  );
-};
-
 export default Page;
