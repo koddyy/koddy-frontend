63e68f8e5a24c62e8d5f0d29e841dfcfe190e405
kwonjuhee <77ooee@gmail.com>
2024-01-21 12:51:49 +0900

    style: Tag 스타일 조정

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -1,35 +1,38 @@
 import { cva, VariantProps } from "cva";
 import { PropsWithChildren } from "react";
 
-const TagVariants = cva("label rounded-[20px] px-[10px] py-[6px] text-gray-700", {
-  variants: {
-    variant: {
-      solid: "",
-      outline: "shadow-[0_0_0_1px_inset]",
+const TagVariants = cva(
+  "body-3 inline-block h-[32px] rounded-[20px] px-[10px] py-[6px] text-gray-700",
+  {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "shadow-[0_0_0_1px_inset]",
+      },
+      color: {
+        "primary-dark": "",
+        primary: "",
+        grayscale: "",
+      },
     },
-    color: {
-      "primary-dark": "",
-      primary: "",
-      grayscale: "",
+    compoundVariants: [
+      { variant: "solid", color: "primary", className: "bg-primary text-white" },
+      { variant: "solid", color: "primary-dark", className: "bg-primary-dark text-white" },
+      { variant: "solid", color: "grayscale", className: "bg-gray-100 text-gray-700" },
+      { variant: "outline", color: "primary", className: "text-gray-700 shadow-primary" },
+      {
+        variant: "outline",
+        color: "primary-dark",
+        className: "text-gray-700 shadow-primary-dark",
+      },
+      { variant: "outline", color: "grayscale", className: "text-gray-700 shadow-gray-300" },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      color: "primary",
     },
-  },
-  compoundVariants: [
-    { variant: "solid", color: "primary", className: "bg-primary text-white" },
-    { variant: "solid", color: "primary-dark", className: "bg-primary-dark text-white" },
-    { variant: "solid", color: "grayscale", className: "bg-gray-100 text-gray-700" },
-    { variant: "outline", color: "primary", className: "text-gray-700 shadow-primary" },
-    {
-      variant: "outline",
-      color: "primary-dark",
-      className: "text-gray-700 shadow-primary-dark",
-    },
-    { variant: "outline", color: "grayscale", className: "text-gray-700 shadow-gray-300" },
-  ],
-  defaultVariants: {
-    variant: "solid",
-    color: "primary",
-  },
-});
+  }
+);
 
 interface TagProps extends VariantProps<typeof TagVariants> {}
 
