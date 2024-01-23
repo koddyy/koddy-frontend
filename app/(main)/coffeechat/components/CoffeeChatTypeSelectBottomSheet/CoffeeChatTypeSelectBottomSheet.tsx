9cc2351aab98807a0b71aa59286999a117623c60
kwonjuhee <77ooee@gmail.com>
2024-01-23 13:25:00 +0900

    feat: SNS 선택 버튼 추가

--- app/(main)/coffeechat/components/CoffeeChatTypeSelectBottomSheet/CoffeeChatTypeSelectBottomSheet.tsx
+++ app/(main)/coffeechat/components/CoffeeChatTypeSelectBottomSheet/CoffeeChatTypeSelectBottomSheet.tsx
@@ -7,8 +7,12 @@ import {
   CoffeeChatType,
   CoffeeChatTypeIcon,
   CoffeeChatTypeLabel,
-  CoffeeChatTypeOptions,
+  Meeting,
+  MeetingOptions,
+  SNS,
+  SNSOptions,
 } from "@/constants/coffeechat";
+import { cn } from "@/utils/cn";
 
 interface CoffeeChatTypeSelectBottomSheetProps extends BottomSheetProps {
   onSubmit: ({ chatType, chatValue }: { chatType: CoffeeChatType; chatValue: string }) => void;
@@ -18,8 +22,11 @@ export const CoffeeChatTypeSelectBottomSheet = ({
   onClose,
   onSubmit,
 }: CoffeeChatTypeSelectBottomSheetProps) => {
-  const [chatType, setChatType] = useState<CoffeeChatType>();
+  const [selectedType, setSelectedType] = useState<Meeting | "SNS ID">();
+  const [seletedSNS, setseletedSNS] = useState<SNS>();
   const [chatValue, setChatValue] = useState("");
+
+  const chatType = selectedType === "SNS ID" ? seletedSNS : selectedType;
 
   const isDisabled = !chatType || !chatValue;
 
@@ -30,7 +37,7 @@ export const CoffeeChatTypeSelectBottomSheet = ({
 
   return (
     <BottomSheet onClose={onClose}>
-      <div className="mb-[12px] flex flex-col gap-[12px]">
+      <div className="mb-[12px] flex h-[228px] flex-col gap-[12px]">
         <div>
           <div className="subheading-bold mb-[4px]">커피챗 방식</div>
           <div className="body-2 text-gray-600">
@@ -39,10 +46,26 @@ export const CoffeeChatTypeSelectBottomSheet = ({
         </div>
         <Select
           placeholder="커피챗 방식 선택"
-          options={CoffeeChatTypeOptions}
-          value={chatType}
-          onChangeValue={setChatType}
+          options={[...MeetingOptions, "SNS ID"]}
+          value={selectedType}
+          onChangeValue={setSelectedType}
+          renderValue={(value) => {
+            if (!value) return null;
+            if (value === "SNS ID") return "SNS ID";
+
+            const Icon = CoffeeChatTypeIcon[value];
+            return (
+              <div className="flex gap-[6px]">
+                {Icon && <Icon />}
+                {CoffeeChatTypeLabel[value]}
+              </div>
+            );
+          }}
           renderOption={(value) => {
+            if (value === "SNS ID") {
+              return <div className="py-[8px]">SNS ID</div>;
+            }
+
             const Icon = CoffeeChatTypeIcon[value];
             return (
               <div className="flex gap-[6px] py-[8px]">
@@ -52,7 +75,27 @@ export const CoffeeChatTypeSelectBottomSheet = ({
             );
           }}
         />
-        <TextArea height="sm" value={chatValue} onChange={(e) => setChatValue(e.target.value)} />
+        {selectedType === "SNS ID" && (
+          <div className="flex gap-[14px]">
+            {SNSOptions.map((SNS) => (
+              <button
+                key="sns"
+                className={cn(
+                  "body-2 w-full rounded-[10px] border border-gray-200 px-[18px] py-[11px] text-gray-500 transition",
+                  SNS === seletedSNS && "body-2-bold border-primary-dark text-primary-dark"
+                )}
+                onClick={() => setseletedSNS(SNS)}
+              >
+                {CoffeeChatTypeLabel[SNS]}
+              </button>
+            ))}
+          </div>
+        )}
+        <TextArea
+          className="h-full"
+          value={chatValue}
+          onChange={(e) => setChatValue(e.target.value)}
+        />
       </div>
       <ButtonArea>
         <Button disabled={isDisabled} onClick={handleClickNext}>
