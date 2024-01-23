d009401793e97f11585ff04dcf83cff433f54319
kwonjuhee <77ooee@gmail.com>
2024-01-24 00:05:08 +0900

    feat: locale에 따라 document language 설정

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+import QueryClientProvider from "@/app/providers/QueryClientProvider";
+import MswInit from "@/mocks/MswInit";
+
+export default function RootLayout({
+  params: { locale },
+  children,
+}: {
+  params: { locale: string };
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang={locale}>
+      <head>
+        <link rel="icon" href="favicon.svg" />
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin=""
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.css"
+        />
+        <title>코띠</title>
+      </head>
+      <body className="relative mx-auto min-h-screen min-w-[375px] max-w-screen-sm">
+        <MswInit />
+        <QueryClientProvider>{children}</QueryClientProvider>
+      </body>
+    </html>
+  );
+}
