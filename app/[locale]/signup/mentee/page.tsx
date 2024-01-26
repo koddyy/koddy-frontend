a6661a2eb076f9a040d8b86bbe06030c44957585
kwonjuhee <77ooee@gmail.com>
2024-01-26 17:24:00 +0900

    feat: 회원가입 요청시 스토어에 저장된 provider, social id를 추가로 넘긴다

--- app/[locale]/signup/mentee/page.tsx
+++ app/[locale]/signup/mentee/page.tsx
@@ -28,7 +28,7 @@ const Page = () => {
     },
   });
   const { user } = useUserStore();
-  const { setLoggedIn } = useProviderStore();
+  const { provider, socialId, setLoggedIn } = useProviderStore();
   const { mutate: signup } = useSignupAsMentee();
 
   const handleClickGoback = () => {
@@ -41,10 +41,10 @@ const Page = () => {
   };
 
   const onSubmitForm = (data: ISignupForm) => {
-    if (!user) return;
+    if (!user || !provider || !socialId) return;
 
     signup(
-      { ...user, ...data },
+      { provider, socialId, ...user, ...data },
       {
         onSuccess: () => {
           handleClickNextStep();
