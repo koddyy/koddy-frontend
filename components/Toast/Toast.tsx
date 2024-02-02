f7f0219ed3763d200347c01f4288b284bf91e51b
kwonjuhee <77ooee@gmail.com>
2024-02-02 14:37:44 +0900

    style: Toast 스타일 조정

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -14,11 +14,11 @@ export const Toast = ({ type = "default", content, onClose }: ToastProps) => {
 
   return (
     <div
-      className="flex w-full items-center rounded bg-gray-100 shadow"
+      className="flex w-full items-center rounded bg-gray-200 p-[10px] shadow-lg"
       data-state={isOpen ? "open" : "close"}
     >
-      {type === "error" && <div className="text-danger">ERROR</div>}
-      <span className="p-[10px]">{content}</span>
+      {type === "error" && <div className="mr-[10px] text-danger">ERROR</div>}
+      <span>{content}</span>
       {onClose && (
         <button className="ml-auto p-[10px] text-gray-700" type="button" onClick={close}>
           <Close width={16} height={16} />
