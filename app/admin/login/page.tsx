3467d4b227225f9cf49087606459c62794fcfe2e
kwonjuhee <77ooee@gmail.com>
2024-02-02 20:05:58 +0900

    feat: root layout 파일 이동 및 admin 페이지는 i18n 적용 제외

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+
+const Page = () => {
+  return (
+    <>
+      <Input />
+      <Input />
+      <Button></Button>
+    </>
+  );
+};
+
+export default Page;
