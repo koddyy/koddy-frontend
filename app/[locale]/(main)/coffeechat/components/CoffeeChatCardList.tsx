3d07843d599e7a7f1e909580c74d3cde7092c319
kwonjuhee <77ooee@gmail.com>
2024-02-02 12:57:03 +0900

    feat: 커피챗 목록 empty 화면 추가

--- app/[locale]/(main)/coffeechat/components/CoffeeChatCardList.tsx
+++ app/[locale]/(main)/coffeechat/components/CoffeeChatCardList.tsx
@@ -26,6 +26,8 @@ export const CoffeeChatCardListWithMentor = ({ category, status }: CoffeeChatCar
     if (hasNextPage && !isFetching) fetchNextPage();
   });
 
+  if (coffeeChatListWithMentor.length === 0) return <Empty text="멘토 보러 가기" />;
+
   return (
     <>
       {coffeeChatListWithMentor.map(({ id, status, mentorId, ...mentor }) => {
@@ -55,6 +57,8 @@ export const CoffeeChatCardListWithMentee = ({ category, status }: CoffeeChatCar
     if (hasNextPage && !isFetching) fetchNextPage();
   });
 
+  if (coffeeChatListWithMentee.length === 0) return <Empty text="멘티 보러 가기" />;
+
   return (
     <>
       {coffeeChatListWithMentee.map(({ id, status, menteeId, ...mentee }) => {
@@ -66,5 +70,20 @@ export const CoffeeChatCardListWithMentee = ({ category, status }: CoffeeChatCar
       })}
       <div ref={ref} />
     </>
+  );
+};
+
+const Empty = ({ text }: { text: string }) => {
+  return (
+    <div className="mt-[80px] flex flex-col items-center">
+      <img className="h-[136px] w-[165px]" src="/images/illustration_no_coffee_chat.svg" />
+      <div className="mb-[12px] mt-[17px]">커피챗이 없어요</div>
+      <Link
+        href="/"
+        className="body-1-bold flex h-[44px] w-[237px] items-center justify-center rounded-[10px] border border-primary text-gray-600"
+      >
+        {text}
+      </Link>
+    </div>
   );
 };
