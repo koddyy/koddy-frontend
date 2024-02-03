691f021dd41a6df1fbcb3fe7b8957dfea681ba48
kwonjuhee <77ooee@gmail.com>
2024-02-03 17:52:07 +0900

    feat: 비로그인 유저에게 내일정, 마이페이지 콘텐츠 접근 제한 처리

--- app/[locale]/(main)/mypage/page.tsx
+++ app/[locale]/(main)/mypage/page.tsx
@@ -1,34 +1,42 @@
 "use client";
 
 import Link from "next/link";
-import { useGetMe } from "@/apis/user/hooks/useGetMe";
 import { NavigationBar } from "@/app/components/NavigationBar";
 import ThreeDots from "@/assets/three_dots.svg";
+import { Button } from "@/components/Button";
 import { Divider } from "@/components/Divider/Divider";
 import { PATH } from "@/constants/path";
+import { DefaultProfileImageUrl } from "@/constants/profile";
+import { useAuth } from "@/hooks/useAuth";
 import { cn } from "@/utils/cn";
 
 const linkStyle = "inline-block w-full px-[20px] py-[14px]";
 
-const Page = () => {
-  const { data: me } = useGetMe();
+const Page = ({
+  searchParams,
+}: {
+  searchParams: {
+    explore?: string;
+  };
+}) => {
+  const explore = searchParams.explore ?? "mentee";
+  const { isAuthenticated, me } = useAuth();
 
-  if (!me) return;
-
-  const isMentor = me.role === "mentor";
-
-  const description = isMentor
-    ? `${me.school} ${me.major} ${me.enteredIn}학번`
-    : `관심 : ${me.interestSchool}, ${me.interestMajor}`;
+  const description =
+    me?.role === "mentor"
+      ? `${me.school} ${me.major} ${me.enteredIn}학번`
+      : `관심 : ${me?.interestSchool}, ${me?.interestMajor}`;
 
   return (
     <>
       <NavigationBar
         title="마이페이지"
         rightContent={
-          <Link href={PATH.MYPAGE_ACCOUNT}>
-            <ThreeDots />
-          </Link>
+          isAuthenticated && (
+            <Link href={PATH.MYPAGE_ACCOUNT}>
+              <ThreeDots />
+            </Link>
+          )
         }
       />
       <div className="flex flex-col items-center gap-3 px-11 pb-5 pt-[1.38rem]">
@@ -37,34 +45,56 @@ const Page = () => {
             "h-[6.75rem] w-[6.75rem] rounded-xl object-cover",
             "border border-gray-300 bg-gray-100 object-contain p-[0.3rem]"
           )}
-          src={isMentor ? "/images/empty_mentor.svg" : "/images/empty_mentee.svg"}
+          src={
+            isAuthenticated
+              ? me.profileImageUrl || DefaultProfileImageUrl[me.role]
+              : DefaultProfileImageUrl[explore === "mentor" ? "mentee" : "mentor"]
+          }
         />
-        <div className="flex flex-col items-center gap-1">
-          <span className="headline-2">{me.name}</span>
-          <p className="text-gray-600">{description}</p>
-        </div>
-      </div>
-      <Divider className="border-4 border-gray-100" />
-      <div>
-        <Link href={PATH.MYPAGE_EDIT} className={linkStyle}>
-          회원 정보 수정
-        </Link>
-        <Divider className="border-gray-100" />
-        {isMentor && (
-          <Link href={PATH.MYPAGE_EDIT + "/schedule"} className={linkStyle}>
-            커피챗 기간 수정
+        {isAuthenticated ? (
+          <div className="flex flex-col items-center gap-1">
+            <span className="headline-2">{me.name}</span>
+            <p className="text-gray-600">{description}</p>
+          </div>
+        ) : (
+          <Link href={PATH.LOGIN}>
+            <Button
+              variant="outline"
+              size="sm"
+              className="body-1-bold h-[44px] w-[237px] text-gray-600"
+            >
+              로그인/회원가입 하기
+            </Button>
           </Link>
         )}
       </div>
       <Divider className="border-4 border-gray-100" />
-      <div>
-        <Link href={PATH.MYPAGE_EDIT + "/language-and-timezone"} className={linkStyle}>
-          언어 & 타임존 설정
-        </Link>
-        <Divider className="border-gray-100" />
-        <div className="px-5 py-[0.88rem]">문의하기</div>
-        <Divider className="border-gray-100" />
-      </div>
+      {isAuthenticated ? (
+        <>
+          <div>
+            <Link href={PATH.MYPAGE_EDIT} className={linkStyle}>
+              회원 정보 수정
+            </Link>
+            <Divider className="border-gray-100" />
+            {me?.role === "mentor" && (
+              <Link href={PATH.MYPAGE_EDIT + "/schedule"} className={linkStyle}>
+                커피챗 기간 수정
+              </Link>
+            )}
+          </div>
+          <Divider className="border-4 border-gray-100" />
+          <div>
+            <Link href={PATH.MYPAGE_EDIT + "/language-and-timezone"} className={linkStyle}>
+              언어 & 타임존 설정
+            </Link>
+            <Divider className="border-gray-100" />
+            <div className="px-5 py-[0.88rem]">문의하기</div>
+            <Divider className="border-gray-100" />
+          </div>
+        </>
+      ) : (
+        <div className={linkStyle}>문의하기</div>
+      )}
     </>
   );
 };
