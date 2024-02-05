ef70dcdb37067f591d27504a69a5aa700af30cc5
kwonjuhee <77ooee@gmail.com>
2024-02-05 14:22:16 +0900

    fix: 쿠키값 사용으로 인한 hydration 오류 수정

--- app/[locale]/edit/language-and-timezone/page.tsx
+++ app/[locale]/edit/language-and-timezone/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BottomButton } from "@/app/components/BottomButton";
 import { NavigationBar } from "@/app/components/NavigationBar";
 import { FormControl, FormLabel } from "@/components/FormControl";
@@ -9,6 +9,7 @@ import { localesOptions } from "@/constants/locale";
 import { PATH } from "@/constants/path";
 import { timezones } from "@/constants/timezone";
 import { useRouter } from "@/libs/navigation";
+import { SupportLocale } from "@/types/locale";
 import { localeCookie } from "@/utils/locale";
 import { getKeys } from "@/utils/object";
 import { timezoneCookie } from "@/utils/timezone";
@@ -19,14 +20,21 @@ const _timezonesOptions = getKeys(timezones);
 
 const Page = () => {
   const router = useRouter();
-  const [locale, setLocale] = useState(localeCookie.get());
-  const [timezone, setTimezone] = useState(timezoneCookie.get());
+  const [locale, setLocale] = useState<SupportLocale>();
+  const [timezone, setTimezone] = useState<string>();
 
   const handleChangeLanguageAndTimezone = () => {
-    localeCookie.set(locale);
-    timezoneCookie.set(timezone);
-    router.replace(PATH.MYPAGE, { locale });
+    if (locale && timezone) {
+      localeCookie.set(locale);
+      timezoneCookie.set(timezone);
+      router.replace(PATH.MYPAGE, { locale });
+    }
   };
+
+  useEffect(() => {
+    setLocale(localeCookie.get());
+    setTimezone(timezoneCookie.get());
+  }, []);
 
   return (
     <>
