80485b6710311a1c78c3b4819b1a4a4f195256f7
kwonjuhee <77ooee@gmail.com>
2024-02-09 00:24:18 +0900

    feat: 회원 유형 선택 페이지 메세지 추가

--- app/[locale]/signup/components/UserTypeCard/UserTypeCard.tsx
+++ app/[locale]/signup/components/UserTypeCard/UserTypeCard.tsx
@@ -1,4 +1,4 @@
-import { ElementType } from "react";
+import { useTranslations } from "next-intl";
 import Mentee from "@/assets/mentee.svg";
 import Mentor from "@/assets/mentor.svg";
 import { cn } from "@/utils/cn";
@@ -11,29 +11,15 @@ export interface UserTypeCardProps {
   isSelected?: boolean;
 }
 
-type ContentType = {
-  image: ElementType;
-  typeName: string;
-  description: string[];
-};
-
-const contents: {
-  [key in UserType]: ContentType;
-} = {
-  mentor: {
-    image: Mentor,
-    typeName: "멘토",
-    description: ["한국에 적응하고 싶은 유학생들을 위해", "코띠의 멘토가 되어주세요!"],
-  },
-  mentee: {
-    image: Mentee,
-    typeName: "멘티",
-    description: ["한국 생활을 알고 싶다면", "코띠 멘토의 도움을 받아보세요!"],
-  },
+const image = {
+  mentor: Mentor,
+  mentee: Mentee,
 };
 
 export const UserTypeCard = ({ type, onClick, isSelected = false }: UserTypeCardProps) => {
-  const content = contents[type];
+  const t = useTranslations("signup.UserTypeCard");
+
+  const Image = image[type];
 
   return (
     <div
@@ -41,11 +27,15 @@ export const UserTypeCard = ({ type, onClick, isSelected = false }: UserTypeCard
       onClick={onClick}
     >
       <div className="mb-[0.375rem] flex items-center justify-center">
-        <content.image />
+        <Image alt={t(`${type}.illustration-alt-text`)} />
       </div>
       <div className="text-center">
-        <span className="subheading-bold">{content.typeName}</span>
-        <p className="body-3 whitespace-pre-line text-gray-600">{content.description.join("\n")}</p>
+        <span className="subheading-bold">{t(`${type}.roleName`)}</span>
+        <p className="body-3 whitespace-pre-line text-gray-600">
+          {t.rich(`${type}.description`, {
+            br: () => <br />,
+          })}
+        </p>
       </div>
     </div>
   );
