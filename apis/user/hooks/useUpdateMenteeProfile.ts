aa5a64f5424e7235b64de45fff3ecef9b7ceaff5
kwonjuhee <77ooee@gmail.com>
2024-02-15 22:27:30 +0900

    feat: 프로필 완성 api 스펙 변경사항 적용

--- apis/user/hooks/useUpdateMenteeProfile.ts
+++ apis/user/hooks/useUpdateMenteeProfile.ts
@@ -1,11 +1,28 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { fileApi } from "@/apis/file/api";
+import { Mentee } from "@/types/mentee";
 import { userApi } from "../api";
 
 export const useUpdateMenteeProfile = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: userApi.patchMenteeProfile,
+    mutationFn: async ({
+      introduction,
+      profileImageFile,
+    }: Pick<Mentee, "introduction"> & { profileImageFile?: File }) => {
+      if (profileImageFile) {
+        const { preSignedUrl, uploadFileUrl } = await fileApi.getPresignedUrl(
+          profileImageFile.name
+        );
+
+        await fileApi.uploadImageFile({ preSignedUrl, file: profileImageFile });
+
+        return await userApi.patchMenteeProfile({ introduction, profileImageUrl: uploadFileUrl });
+      }
+
+      return userApi.patchMenteeProfile({ introduction });
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["me"] });
     },
