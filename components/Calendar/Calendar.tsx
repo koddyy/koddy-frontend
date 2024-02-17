649f49911b4563d6ee1e5f7a88fb81ed2a7f0815
kwonjuhee <77ooee@gmail.com>
2024-02-17 18:59:50 +0900

    feat: month 국제화 적용

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,17 +1,22 @@
 import "./Calendar.css";
+import { useLocale } from "next-intl";
 import ReactCalendar, { CalendarProps } from "react-calendar";
 
 const WeekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export const Calendar = ({ ...props }: CalendarProps) => {
+  const locale = useLocale();
+
   return (
     <ReactCalendar
-      locale="ko"
+      locale={locale}
       minDetail="month"
       calendarType="gregory"
       minDate={new Date()}
       formatDay={(_, date) => `${date.getDate()}`}
-      formatMonthYear={(_, date) => `${date.getMonth() + 1}월`}
+      navigationLabel={({ date, locale }) =>
+        new Intl.DateTimeFormat(locale, { month: "long" }).format(date)
+      }
       formatShortWeekday={(_, date) => `${WeekDays[date.getDay()]}`}
       next2Label={null}
       prev2Label={null}
