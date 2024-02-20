b0d53bdd78ceb147e7b5af855439372e6839bcdf
kwonjuhee <62097867+kwonjuhee@users.noreply.github.com>
2024-02-21 01:02:57 +0900

    Revert "refactor: 토큰 처리 로직 변경 (#137)"
    This reverts commit c8e70629ab788fd2469633ac97ff99b17e6b65a1.

--- apis/auth/hooks/useOauthLogout.ts
+++ apis/auth/hooks/useOauthLogout.ts
@@ -1,16 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "@/libs/navigation";
-import { authCookie } from "@/stores/auth";
+import { useAuthStore } from "@/stores/auth";
 import { authApi } from "../api";
 
 export const useOauthLogout = () => {
   const router = useRouter();
+  const clear = useAuthStore((state) => state.clear);
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: authApi.oauthLogout,
     onSuccess: () => {
-      authCookie.clear();
+      clear();
       queryClient.removeQueries();
       router.push("/login");
     },
--- apis/user/hooks/useSignout.ts
+++ apis/user/hooks/useSignout.ts
@@ -1,16 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "@/libs/navigation";
-import { authCookie } from "@/stores/auth";
+import { useAuthStore } from "@/stores/auth";
 import { userApi } from "../api";
 
 export const useSignout = () => {
   const router = useRouter();
+  const clear = useAuthStore((state) => state.clear);
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: userApi.signout,
     onSuccess: () => {
-      authCookie.clear();
+      clear();
       queryClient.removeQueries();
       router.push("/login");
     },
