682d823f36f1b47768f716800f41649e6ff0a212
kwonjuhee <77ooee@gmail.com>
2024-02-21 00:53:09 +0900

    refactor: 신청/1차수락 커피챗 수락 hook 분리

---
+++ app/[locale]/(main)/coffeechat/hooks/useApplyToApproveCoffeeChat.ts
@@ -0,0 +1,25 @@
+import { useUpdateCoffeeChatApplyToApprove } from "@/apis/coffeechat-status/hooks/useUpdateCoffeeChatApplyToApprove";
+import { useToggle } from "@/hooks/useToggle";
+
+export const useApplyToApproveCoffeeChat = () => {
+  const [isApprove, toggleIsApprove, setIsApprove] = useToggle();
+  const { mutate: approveCoffeeChat, isSuccess: isApproveSuccess } =
+    useUpdateCoffeeChatApplyToApprove();
+
+  const setIsApproveTrue = () => {
+    setIsApprove(true);
+  };
+
+  const setIsApproveFalse = () => {
+    setIsApprove(false);
+  };
+
+  return {
+    isApprove,
+    isApproveSuccess,
+    toggleIsApprove,
+    setIsApproveTrue,
+    setIsApproveFalse,
+    approveCoffeeChat,
+  };
+};
