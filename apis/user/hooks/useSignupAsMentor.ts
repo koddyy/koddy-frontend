50a5079b3f8ced21ef3739797b562d96f6ac4d44
kwonjuhee <77ooee@gmail.com>
2024-03-01 17:35:40 +0900

    fix: 회원가입 성공시 토큰 저장 로직 추가

--- apis/user/hooks/useSignupAsMentor.ts
+++ apis/user/hooks/useSignupAsMentor.ts
@@ -1,8 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
+import { AxiosHeaders } from "axios";
 import { userApi } from "@/apis/user/api";
+import { authCookie } from "@/stores/auth";
 
 export const useSignupAsMentor = () => {
   return useMutation({
     mutationFn: userApi.signupAsMentor,
+    onSuccess: (response) => {
+      if (response.headers instanceof AxiosHeaders) {
+        const accessToken = response.headers.get("Authorization");
+
+        if (accessToken && typeof accessToken === "string") {
+          authCookie.set(accessToken);
+        }
+      }
+    },
   });
 };
