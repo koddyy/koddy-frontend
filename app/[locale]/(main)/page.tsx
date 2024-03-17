a2b34b1285b68e1573a84eb9835b0d315f9284f3
kwonjuhee <77ooee@gmail.com>
2024-03-17 17:50:04 +0900

    refactor: isLoading -> isPending, 로딩 조건부 수정

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -21,47 +21,53 @@ const Home = ({ searchParams }: { searchParams: { explore?: string } }) => {
   const explore = searchParams.explore ?? "mentee";
   const { createQueryString } = useQueryString();
 
-  const { isLoading, isAuthenticated, me } = useAuth();
-
-  if (isLoading) return null;
+  const { isPending, isAuthenticated, me } = useAuth();
 
   return (
     <>
       <Header
         rightContent={
-          !isAuthenticated ? (
-            <Link href={createQueryString({ explore: explore === "mentor" ? "mentee" : "mentor" })}>
-              <Button size="xs" className="body-2-bold h-[35px]" fullWidth={false}>
-                {explore === "mentor" ? t("explore-mentee") : t("explore-mentor")}
-              </Button>
-            </Link>
-          ) : null
+          !isPending ? (
+            !isAuthenticated ? (
+              <Link
+                href={createQueryString({ explore: explore === "mentor" ? "mentee" : "mentor" })}
+              >
+                <Button size="xs" className="body-2-bold h-[35px]" fullWidth={false}>
+                  {explore === "mentor" ? t("explore-mentee") : t("explore-mentor")}
+                </Button>
+              </Link>
+            ) : null
+          ) : (
+            <></>
+          )
         }
       />
-      <div className="mt-[18px]">
-        {isAuthenticated && !me.profileComplete && (
-          <div className="mb-[26px] px-[20px]">
-            <ProfileCompleteBanner />
-          </div>
-        )}
-        {isAuthenticated && (
-          <SSRSafeSuspense>
-            {me.role === "mentee" && <NewSuggestedCoffeeChatList />}
-            {me.role === "mentor" && <NewAppliedCoffeeChatList />}
-          </SSRSafeSuspense>
-        )}
-        {isAuthenticated ? (
-          <div className="px-[20px]">
-            {me.role === "mentor" && <BrowseMenteeList />}
-            {me.role === "mentee" && <BrowseMentorList />}
-          </div>
-        ) : (
-          <div className="px-[20px]">
-            {explore === "mentee" && <BrowseMenteeList />}
-            {explore === "mentor" && <BrowseMentorList />}
-          </div>
-        )}
-      </div>
+      {!isPending && (
+        <div className="mt-[18px]">
+          {isAuthenticated ? (
+            <>
+              {!me.profileComplete && (
+                <div className="mb-[26px] px-[20px]">
+                  <ProfileCompleteBanner />
+                </div>
+              )}
+              <SSRSafeSuspense>
+                {me.role === "mentee" && <NewSuggestedCoffeeChatList />}
+                {me.role === "mentor" && <NewAppliedCoffeeChatList />}
+              </SSRSafeSuspense>
+              <div className="px-[20px]">
+                {me.role === "mentor" && <BrowseMenteeList />}
+                {me.role === "mentee" && <BrowseMentorList />}
+              </div>
+            </>
+          ) : (
+            <div className="px-[20px]">
+              {explore === "mentee" && <BrowseMenteeList />}
+              {explore === "mentor" && <BrowseMentorList />}
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
